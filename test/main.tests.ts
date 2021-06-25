f34b0f8c366ed6d6f234c1016c49f7c4fdb85140
Deepak Dahiya <59823596+t-dedah@users.noreply.github.com>
2021-06-25 17:42:22 +0000

    Updated test logic

--- test/main.tests.ts
+++ test/main.tests.ts
@@ -2,19 +2,23 @@ import { main } from "../src/main";
 
 // Unit Tests
 export async function runTests() {
-    let result = await main()
-    if(result)
-    {
-        console.log(result)
-    }
-
-    if(process.env.EXPECTED_TO === 'fail'){
-        throw new Error('Expected failure but deployement passed.')
+    try {
+        let result = await main()
+        if (result) {
+            console.log(result)
+        }
+        return 'pass'
+    } catch (e) {
+        return 'fail'
     }
 }
 
-runTests().catch(e => {
-    if(process.env.EXPECTED_TO === 'pass'){
+runTests().then(outcome => {
+    if(outcome != process.env.EXPECTED_TO){
+        throw new Error(`Expected outcome did not meet the real outcome. Expected value: ${process.env.EXPECTED_TO}, actual value: ${outcome}`)
+    }
+}).catch(e => {
+    if (process.env.EXPECTED_TO === 'pass') {
         console.error(e)
         process.exit(1)
     }
