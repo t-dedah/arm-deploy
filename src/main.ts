99170792e1ccfdb48ab9d0c48f5cf078365657eb
Deepak Dahiya <59823596+t-dedah@users.noreply.github.com>
2021-06-28 01:13:12 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { exec } from '@actions/exec';
 import { DeployManagementGroupScope } from './deploy/scope_managementgroup';
 import { DeploySubscriptionScope } from './deploy/scope_subscription';
 import { Outputs } from './utils/utils';
-import { getInput } from '@actions/core';
+import { getInput,getBooleanInput } from '@actions/core';
 
 // Action Main code
 export async function main(): Promise<Outputs> {
@@ -22,38 +22,42 @@ export async function main(): Promise<Outputs> {
     const deploymentName = getInput('deploymentName')
     const parameters = getInput('parameters')
     const managementGroupId = getInput('managementGroupId')
-    const failOnStdErr = getInput('failOnStdErr')
+    const failOnStdErr = getBooleanInput('failOnStdErr') || true
 
     // change the subscription context
     if (scope !== "managementgroup" && subscriptionId !== "") {
         info("Changing subscription context...")
         await exec(`"${azPath}" account set --subscription ${subscriptionId}`, [], { silent: true })
     }
+    
+    console.log(failOnStdErr)
+    console.log(typeOf(failOnStdErr))
+    
 
     // Converting string to bool
-    let failOnStdErrBool: Boolean
-    switch (failOnStdErr.toLowerCase().trim()) {
-        case "true":
-            failOnStdErrBool = true
-            break
-        case "false":
-            failOnStdErrBool = false
-            break
-        default:
-            failOnStdErrBool = true
-    }
+//     let failOnStdErrBool: Boolean
+//     switch (failOnStdErr.toLowerCase().trim()) {
+//         case "true":
+//             failOnStdErrBool = true
+//             break
+//         case "false":
+//             failOnStdErrBool = false
+//             break
+//         default:
+//             failOnStdErrBool = true
+//     }
 
     // Run the Deployment
     let result: Outputs = {};
     switch (scope) {
         case "resourcegroup":
-            result = await DeployResourceGroupScope(azPath, resourceGroupName, template, deploymentMode, deploymentName, parameters, failOnStdErrBool)
+            result = await DeployResourceGroupScope(azPath, resourceGroupName, template, deploymentMode, deploymentName, parameters, failOnStdErr)
             break
         case "managementgroup":
-            result = await DeployManagementGroupScope(azPath, region, template, deploymentMode, deploymentName, parameters, managementGroupId, failOnStdErrBool)
+            result = await DeployManagementGroupScope(azPath, region, template, deploymentMode, deploymentName, parameters, managementGroupId, failOnStdErr)
             break
         case "subscription":
-            result = await DeploySubscriptionScope(azPath, region, template, deploymentMode, deploymentName, parameters, failOnStdErrBool)
+            result = await DeploySubscriptionScope(azPath, region, template, deploymentMode, deploymentName, parameters, failOnStdErr)
             break
         default:
             throw new Error("Invalid scope. Valid values are: 'resourcegroup', 'managementgroup', 'subscription'")
